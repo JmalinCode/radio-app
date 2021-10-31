dc3fd93b9745c429fa3bc27cb53d4a2a6f71219e
JmalinCode <jmalining@ukr.net>
2021-10-31 22:49:43 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+let playPauseButton = document.getElementById('play-pause-btn');
+let volumeRange = document.getElementById("volume-range");
+let volumeButton = document.getElementById('volume-btn');
+let track = document.getElementById('track');
+let volume;
+let audioPlayer = document.getElementById('audioPlayer');
+
+
+playPauseButton.onclick = function (){
+    if (playPauseButton.src.slice(-8) === 'play.png') {
+        audioPlayer.play()
+        playPauseButton.src = 'img/stop.png'
+    } else if (playPauseButton.src.slice(-8) === 'stop.png'){
+        audioPlayer.pause()
+        playPauseButton.src = 'img/play.png'
+    }
+}
+
+volumeButton.onclick = function (){
+    console.log(audioPlayer.currentTime) //delete later
+
+    if (volumeButton.src.slice(-13) === 'volume-on.png') {
+        volumeButton.src = 'img/no-volume.png';
+        volume = volumeRange.value;
+        volumeRange.value = '0';
+    } else if (volumeButton.src.slice(-13) === 'no-volume.png'){
+        volumeButton.src = 'img/volume-on.png';
+        volumeRange.value = volume;
+    }
+}
+
+volumeRange.oninput = function (){
+    audioPlayer.volume = +volumeRange.value/100;
+    if (+volumeRange.value/100 === 0){
+        volumeButton.src = 'img/no-volume.png';
+    } else{
+        volumeButton.src = 'img/volume-on.png';
+    }
+
+}
+
+
